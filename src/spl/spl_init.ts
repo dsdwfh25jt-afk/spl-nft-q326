d3146d0915e3e04e03ba881b86b2e4a9644272bd
dsdwfh25jt-afk <gauravbhat7121@icloud.com>
2026-08-28 13:42:15 +0000

    initialize mint account

--- src/spl/spl_init.ts
+++ src/spl/spl_init.ts
@@ -9,6 +9,7 @@ import {
   createTransactionMessage,
   generateKeyPairSigner,
   getSignatureFromTransaction,
+  lamports,
   sendAndConfirmTransactionFactory,
   setTransactionMessageFeePayerSigner,
   setTransactionMessageLifetimeUsingBlockhash,
@@ -22,7 +23,8 @@ import {
 import { getCreateAccountInstruction } from "@solana-program/system";
 
 //import your wallet
-import wallet from "../../devnet-wallet.json";
+import wallet from "/mnt/d/GuruPT8bAvseqUTpTEXYs913YgAd3LqvTxK3VPMdJ8u5.json";
+import { send } from "node:process";
 
 const rpc = createSolanaRpc("https://api.devnet.solana.com");
 
@@ -32,6 +34,60 @@ const rpcSubscriptions = createSolanaRpcSubscriptions(
 
 (async () => {
   try {
+    const signer = await createKeyPairSignerFromBytes(new Uint8Array(wallet));
+
+    const mint = await generateKeyPairSigner();
+
+    const space =BigInt(getMintSize());
+
+    const rent = await rpc.getMinimumBalanceForRentExemption(space).send();
+
+    const { value : latestBlockhash } = await rpc.getLatestBlockhash().send();
+
+    const sendAndConfirm = sendAndConfirmTransactionFactory({
+      rpc,
+      rpcSubscriptions,
+    });
+
+    const msg = createTransactionMessage({version : 0});
+
+    const msgWithPayer = setTransactionMessageFeePayerSigner(signer, msg);
+
+    const msgWithLifetime = setTransactionMessageLifetimeUsingBlockhash(
+      latestBlockhash,
+      msgWithPayer,
+    );
+
+    const txMessage = appendTransactionMessageInstructions(
+      [
+        getCreateAccountInstruction({
+          payer : signer,
+          newAccount : mint,
+          lamports: rent,
+          space ,
+          programAddress : TOKEN_PROGRAM_ADDRESS,
+        }),
+
+        getInitializeMintInstruction({
+          mint : mint.address,
+          decimals: 6,
+          mintAuthority : signer.address,
+        }),
+      ],
+      msgWithLifetime,
+    );
+
+    const signedTx = await signTransactionMessageWithSigners(txMessage);
+    assertIsTransactionWithBlockhashLifetime(signedTx);
+
+    const signature = getSignatureFromTransaction(signedTx)
+    
+    await sendAndConfirm (signedTx, {commitment : "confirmed"});
+
+    console.log(`
+      mint address : ${mint.address} /n
+      Transaction Signature : ${signature}
+      `);
   } catch (error) {
     console.log(error);
   }
